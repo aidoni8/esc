da6c863f405b513fe9bb101f4ffe5435e0fe0b3f
aidoni8 <150538775+aidoni8@users.noreply.github.com>
2024-02-03 11:11:11 -0600

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,6 +1,6 @@
-# Security Group for ELB
-resource "aws_security_group" "elb_sg" {
-  name        = "elb-security-group"
+# Security Group for LB
+resource "aws_security_group" "lb_sg" {
+  name        = "lb-security-group"
   description = "Security group for Elastic Load Balancer"
   vpc_id      = module.vpc.vpc_id
   
@@ -33,14 +33,15 @@ resource "aws_security_group" "elb_sg" {
 resource "aws_security_group" "ecs_task_sg" {
   name        = "ecs-task-security-group"
   description = "Security group for ECS tasks"
-  vpc_id      = aws_vpc.my_vpc.id
+  vpc_id      = module.vpc.vpc_id
 
-  # Inbound rule to allow traffic from ELB
+  # Inbound rule to allow traffic from LB
   ingress {
-    from_port          = 0
-    to_port            = 65535
-    protocol           = "tcp"
-    security_groups    = [aws_security_group.elb_sg.id]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_groups    = [aws_security_group.lb_sg.id]
   }
 
   # Outbound rule allowing all traffic
