8ecd911e87811b8cf9dee792d9c3128f7c69ab0a
aidoni8 <150538775+aidoni8@users.noreply.github.com>
2024-02-03 11:55:14 -0600

    Update var.tf

--- var.tf
+++ var.tf
@@ -2,3 +2,9 @@ variable "ecs_task_execution_role_name" {
   type    = string
   default = "ecsTaskExecutionRole"
 }
+
+variable "aws_ecs_cluster_name" {
+  type        = string
+  description = "Enter a name for ECS cluster"
+  default     = "demo-ecs-cluster"
+}
