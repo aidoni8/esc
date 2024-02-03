a4c695629b7011b07c172725c7f61a23c93033f7
aidoni8 <150538775+aidoni8@users.noreply.github.com>
2024-02-03 12:47:39 -0600

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,77 +1,35 @@
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
-
-  name    = "my-alb"
-  vpc_id  = "module.vpc.vpc_id"
-  subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  # Security Group
-  security_group_ingress_rules = {
-    all_http = {
-      from_port   = 80
-      to_port     = 80
-      ip_protocol = "tcp"
-      description = "HTTP web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-    all_https = {
-      from_port   = 443
-      to_port     = 443
-      ip_protocol = "tcp"
-      description = "HTTPS web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-  }
-  security_group_egress_rules = {
-    all = {
-      ip_protocol = "-1"
-      cidr_ipv4   = "10.0.0.0/16"
-    }
-  }
-
-  access_logs = {
-    bucket = "my-alb-logs"
-  }
-
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-      forward = {
-        target_group_key = "ex-instance"
-      }
-    }
-  }
-
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "h1"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-    }
-  }
-
-  tags = {
-    Environment = "Development"
-    Project     = "ALB"
-  }
-
-resource "aws_lb_target_group_attachment" "this" {
-  count           = length(keys(module.alb.target_groups))
-  target_group_arn = aws_lb_target_group.my_target_group.arn
-  target_id       = module.ecs_service.task_ids[count.index]
+resource "aws_lb" "my_alb" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]                          # Associate the ALB with the security group
+  subnets            = [for subnet in aws_subnet.public_subnet[*] : subnet.id] # Specify your public subnets
 }
 
+resource "aws_lb_target_group" "my_target_group" {
+  name        = "my-target-group"
+  port        = 80              # Port that your ECS tasks or containers are listening on
+  protocol    = "HTTP"          # Protocol used by your ECS tasks or containers
+  vpc_id      = aws_vpc.main.id # Specify your VPC ID
+  target_type = "ip"
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    timeout             = 5
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb_listener" "my_listener" {
+  load_balancer_arn = aws_lb.my_alb.arn # Specify the ARN of your ALB
+  port              = 80                # Port on which the ALB will listen for incoming traffic
+  protocol          = "HTTP"            # Protocol used by the ALB
+
+  default_action {
+    type             = "forward"                               # Action to take for incoming requests
+    target_group_arn = aws_lb_target_group.my_target_group.arn # Specify the ARN of your target group
+  }
 }
