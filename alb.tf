86989cfd834c5a321da2296017da5146c7a9d668
aidoni8 <150538775+aidoni8@users.noreply.github.com>
2024-02-03 11:45:12 -0600

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,40 +1,70 @@
-# Define ALB
-resource "aws_lb" "my_alb" {
-  name                        = "my-alb"
-  internal                    = false
-  load_balancer_type          = "application"
-  security_groups             = [aws_security_group.alb_sg.id]
-  subnets                     = [aws_subnet.my_subnet.id]
-  enable_deletion_protection  = false
-}
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
 
-# Define ALB listeners
-resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.my_alb.arn
-  port              = 80
-  protocol          = "HTTP"
+  name    = "my-alb"
+  vpc_id  = "module.vpc.vpc_id"
+  subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.my_target_group.arn
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      description = "HTTPS web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
   }
-}
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "10.0.0.0/16"
+    }
+  }
 
-# Define the ALB target group
-resource "aws_lb_target_group" "my_target_group" {
-  name                = "my-target-group"
-  port                = 80
-  protocol            = "HTTP"
-  vpc_id              = aws_vpc.my_vpc.id
-  target_type         = "ip"
-  
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    timeout             = 5
-    interval            = 30
-    healthy_threshold   = 2
-    unhealthy_threshold = 2 
-    matcher             = "200"
+  access_logs = {
+    bucket = "my-alb-logs"
+  }
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "h1"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "ALB"
   }
 }
