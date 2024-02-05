f3f8bcf48e5480631822853cdd7fd76b94340b68
aidoni8 <150538775+aidoni8@users.noreply.github.com>
2024-02-05 17:06:26 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,92 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "project-vpc"
+  }
+}
+#Internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "project-igw"
+  }
+}
+#Public route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
 
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-route-table"
+  }
+}
 
-  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = 3
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+#Public Subnets
+resource "aws_subnet" "public_subnet" {
+  count                   = 3
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
 
   tags = {
-    Terraform = "true"
-    Environment = "dev"
+    Name = "public-subnet-${count.index + 1}"
   }
+}
+
+#Private Route table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+#Private subnets
+
+resource "aws_subnet" "private_subnet" {
+  count                   = 3
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnets[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = false
+
+
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
+resource "aws_route_table_association" "private_subnet_association" {
+  count          = 3
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_eip" "nat_gateway_eip" {
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnet[0].id
+  tags = {
+    Name = "project-nat"
+  }
+}
+
+resource "aws_route" "private_subnet_route_to_nat" {
+  route_table_id         = aws_route_table.private_route_table.id
+  nat_gateway_id         = aws_nat_gateway.nat.id
+  destination_cidr_block = "0.0.0.0/0"
 }
