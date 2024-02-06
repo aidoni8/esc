93bd565ea0e7eb3908b02d4cd5928d6870394c53
aidoni8 <150538775+aidoni8@users.noreply.github.com>
2024-02-05 19:54:57 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,23 @@
+# Define variables
+variable "vpc_cidr_block" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr_blocks" {
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidr_blocks" {
+  default = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+}
+
+variable "availability_zones" {
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
 # Define the VPC
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr_block
   enable_dns_hostnames = true
   enable_dns_support = true
 
@@ -20,10 +37,10 @@ resource "aws_internet_gateway" "my_igw" {
 
 # Create public subnets
 resource "aws_subnet" "public_subnet" {
-  count             = 3
+  count             = length(var.public_subnet_cidr_blocks)
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.${count.index}.0/24"
-  availability_zone = "us-east-2"
+  cidr_block        = var.public_subnet_cidr_blocks[count.index]
+  availability_zone = element(var.availability_zones, count.index)
 
   tags = {
     Name = "PublicSubnet-${count.index}"
@@ -32,10 +49,10 @@ resource "aws_subnet" "public_subnet" {
 
 # Create private subnets
 resource "aws_subnet" "private_subnet" {
-  count             = 3
+  count             = length(var.private_subnet_cidr_blocks)
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.${count.index + 100}.0/24"
-  availability_zone = "us-east-2"
+  cidr_block        = var.private_subnet_cidr_blocks[count.index]
+  availability_zone = element(var.availability_zones, count.index)
 
   tags = {
     Name = "PrivateSubnet-${count.index}"
@@ -54,20 +71,20 @@ resource "aws_route_table" "public_route_table" {
 
 # Associate public subnets with the public route table
 resource "aws_route_table_association" "public_subnet_association" {
-  count          = 3
+  count          = length(aws_subnet.public_subnet)
   subnet_id      = aws_subnet.public_subnet[count.index].id
   route_table_id = aws_route_table.public_route_table.id
 }
 
 # Create route tables for private subnets
 resource "aws_route_table" "private_route_table" {
-  count = 3
+  count = length(aws_subnet.private_subnet)
   vpc_id = aws_vpc.my_vpc.id
 }
 
 # Associate private subnets with the private route table
 resource "aws_route_table_association" "private_subnet_association" {
-  count          = 3
+  count          = length(aws_subnet.private_subnet)
   subnet_id      = aws_subnet.private_subnet[count.index].id
   route_table_id = aws_route_table.private_route_table[count.index].id
 }
