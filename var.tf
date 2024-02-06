6f5ef8350644525d734077b72fab9e27ccf0c705
aidoni8 <150538775+aidoni8@users.noreply.github.com>
2024-02-05 18:45:01 -0600

    Update var.tf

--- var.tf
+++ var.tf
@@ -15,13 +15,13 @@ variable "availability_zones" {
 
 variable "public_subnets" {
   type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  default     = ["10.0.4.0/16", "10.0.4.0/16", "10.0.6.0/16"]
   description = "CIDR blocks for public subnets"
 }
 
 variable "private_subnets" {
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default     = ["10.0.1.0/16", "10.0.2.0/16", "10.0.3.0/16"]
   description = "CIDR blocks for private subnets"
 }
 
